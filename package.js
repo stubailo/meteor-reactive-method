35fbef84cb2f5f522394b80710812a4d69cd06a9
Sashko Stubailo <sashko@mit.edu>
2015-02-13 22:30:03 -0800

    Flesh out package.js

--- package.js
+++ package.js
@@ -1,10 +1,10 @@
 Package.describe({
   name: 'simple:reactive-method',
-  version: '0.0.1',
+  version: '1.0.0',
   // Brief, one-line summary of the package.
-  summary: '',
+  summary: 'Call methods synchronously inside Tracker.autorun',
   // URL to the Git repository containing the source code for this package.
-  git: '',
+  git: 'https://github.com/stubailo/meteor-reactive-method',
   // By default, Meteor will default to using README.md for documentation.
   // To avoid submitting documentation, set this field to null.
   documentation: 'README.md'
