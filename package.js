fa96d59d7b3262db048a76f7165ad80a7a730e2a
Dean Radcliffe <dradcliffe@opinionlab.com>
2015-03-24 20:54:33 -0500

    Change package over to deanius and publish as 1.1.0

--- package.js
+++ package.js
@@ -1,26 +1,20 @@
 Package.describe({
-  name: 'simple:reactive-method',
-  version: '1.0.0',
-  // Brief, one-line summary of the package.
-  summary: 'Call methods synchronously inside Tracker.autorun',
-  // URL to the Git repository containing the source code for this package.
-  git: 'https://github.com/stubailo/meteor-reactive-method',
-  // By default, Meteor will default to using README.md for documentation.
-  // To avoid submitting documentation, set this field to null.
+  name: 'deanius:reactive-method',
+  version: '1.1.0',
+  summary: 'For all practical purposes, make `Meteor.call` sync inside `Tracker.autorun`',
+  git: 'https://github.com/chicagogrooves/meteor-reactive-method',
   documentation: 'README.md'
 });
 
 Package.onUse(function(api) {
   api.versionsFrom('1.0.3.1');
   api.use(["tracker", "meteor", "ddp", "ejson"]);
-
   api.addFiles('reactive-method.js');
-
   api.export("ReactiveMethod");
 });
 
 Package.onTest(function(api) {
   api.use('tinytest');
-  api.use(['simple:reactive-method', 'test-helpers', 'reactive-var']);
+  api.use(['deanius:reactive-method', 'test-helpers', 'reactive-var']);
   api.addFiles('reactive-method-tests.js');
 });
