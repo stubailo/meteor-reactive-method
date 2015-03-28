d6ce55222d19f91ef2f3abc7a879a2ef39dcb256
Sashko Stubailo <sashko@stubailo.com>
2015-03-27 23:33:51 -0700

    Bump version for issue #4

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'simple:reactive-method',
-  version: '1.0.0',
+  version: '1.0.1',
   // Brief, one-line summary of the package.
   summary: 'Call methods synchronously inside Tracker.autorun',
   // URL to the Git repository containing the source code for this package.
