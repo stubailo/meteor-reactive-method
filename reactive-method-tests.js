49fb6f7c9921f73fce4d3c552556698ac07e7015
Sashko Stubailo <sashko@stubailo.com>
2015-04-22 12:26:10 -0700

    Fix up stuff about invalidation

--- reactive-method-tests.js
+++ reactive-method-tests.js
@@ -6,7 +6,7 @@ if (Meteor.isServer) {
   });
 } else {
   testAsyncMulti("throws error outside of autorun", [
-    function (test, expect) {
+    function (test) {
       var error;
       try {
         ReactiveMethod.call("joinStrings", "a", "b");
@@ -16,7 +16,7 @@ if (Meteor.isServer) {
 
       test.equal(error.message, "Don't use ReactiveMethod.call outside of a Tracker computation.");
     },
-    function (test, expect) {
+    function (test) {
       var error;
       try {
         ReactiveMethod.apply("joinStrings", "a", "b");
@@ -39,6 +39,7 @@ if (Meteor.isServer) {
           test.equal(result, "a, b");
 
           setTimeout(function () {
+            computation.stop();
             checkedResult();
           }, 0);
         }
@@ -54,6 +55,7 @@ if (Meteor.isServer) {
           test.equal(result, "a, b");
 
           setTimeout(function () {
+            computation.stop();
             checkedResult();
           }, 0);
         }
@@ -64,23 +66,44 @@ if (Meteor.isServer) {
   testAsyncMulti("avoid extra reruns", [
     function (test, expect) {
       var done = expect();
-      var reruns = 0;
+      var runs = 0;
       var argVar = new ReactiveVar(1);
 
       Tracker.autorun(function () {
         // just to trigger recomputation
         argVar.get();
         ReactiveMethod.call("joinStrings", "a", "b");
-        reruns ++;
+        runs++;
       });
 
-      argVar.set(2);
-      argVar.set(3);
+      setTimeout(function () {
+        argVar.set(2);
+      }, 200);
+
+      setTimeout(function () {
+        argVar.set(3);
+      }, 400);
+
+      setTimeout(function () {
+        ReactiveMethod.invalidateCall("joinStrings", "a", "b");
+      }, 600);
 
       setTimeout(function () {
         // I can't think any way to check how many reruns happen without
         // setting a strict timeout
-        test.equal(reruns, 2);
+        
+        // Here is why there are 6 runs:
+        // 1. The initial run
+        // 2. The method result comes back
+        // 3. argVar is set to 2
+        // 4. argVar is set to 3
+        // 5. Method result is reset by invalidateCall
+        // 6. New method result comes back
+        
+        // There should not be more than 4 runs; if there are then it means
+        // the method call is returning more than once, which means something
+        // about result caching broke
+        test.equal(runs, 6);
         done();
       }, 1000);
     }
--- reactive-method.js
+++ reactive-method.js
@@ -1,18 +1,49 @@
-// Write your package code here!
-
 ReactiveMethod = {
+  /**
+   * A global object that matches serialized arguments of Method.apply to an
+   * array of computations that depend on the result of that method. Used mainly
+   * to allow invalidation of method results from outside of the computation
+   * using that result.
+   * @type {Object}
+   */
   _computations: {},
-  call: function (/* arguments */) {
+
+  /**
+   * Call a Meteor method. Can only be used inside a Tracker autorun (which
+   * includes Blaze helpers). Functions much like a promise - on the first run
+   * of the computation returns undefined, and when the method result comes back
+   * reruns the computation and returns the actual result. If this method is
+   * called in consecutive reruns of the computation with the same arguments, it
+   * remembers the previous result, which avoids calling the method over and
+   * over again forever.
+   *
+   * Watch out - if you call this method with a constantly changing value as one
+   * of the arguments (for example the current time or a random value) it will
+   * never return anything.
+   *
+   * The API for the arguments is exactly the same as Meteor.call.
+   * 
+   * @param {String} methodName The name of the method to call
+   * @param {EJSONable} [arg1,arg2...] Optional method arguments
+   */
+  call: function (methodName /*, ...arguments */) {
     if (! Tracker.currentComputation) {
       // If not in an autorun, throw error
       throw new Error("Don't use ReactiveMethod.call outside of a Tracker computation.");
     }
 
     var args = _.toArray(arguments);
-
-    return ReactiveMethod.apply(_.first(args), _.rest(args));
+    return ReactiveMethod.apply(methodName, _.rest(args));
   },
-  apply: function (/* arguments */) {
+
+  /**
+   * Just like ReactiveMethod.call except uses the calling API of Meteor.apply
+   * instead of Meteor.call. 
+   * @param  {[type]} methodName [description]
+   * @param  {[type]} methodArgs [description]
+   * @return {[type]}            [description]
+   */
+  apply: function (methodName, methodArgs) {
     var cc = Tracker.currentComputation;
 
     if (! cc) {
@@ -20,12 +51,10 @@ ReactiveMethod = {
       throw new Error("Don't use ReactiveMethod.apply outside of a Tracker computation.");
     }
 
-    var args = _.toArray(arguments);
-    var serializedArgs = EJSON.stringify(args);
+    var serializedArgs = EJSON.stringify([methodName, methodArgs]);
 
-    ReactiveMethod._computations[serializedArgs] = ReactiveMethod._computations[serializedArgs] || [];
-    ReactiveMethod._computations[serializedArgs].push(cc);
-    ReactiveMethod._computations[serializedArgs] = _.uniq(ReactiveMethod._computations[serializedArgs]);
+    recordMethodComputation(cc, serializedArgs);
+
     cc._reactiveMethodData = cc._reactiveMethodData || {};
     cc._reactiveMethodStale = cc._reactiveMethodStale || {};
 
@@ -39,7 +68,7 @@ ReactiveMethod = {
       return cc._reactiveMethodData[serializedArgs];
     }
 
-    Meteor.apply(args[0], args[1], function (err, result) {
+    Meteor.apply(methodName, methodArgs, function (err, result) {
       cc._reactiveMethodData[serializedArgs] = result;
       cc.invalidate();
     });
@@ -54,23 +83,81 @@ ReactiveMethod = {
           if (cc._reactiveMethodStale[serializedArgs]) {
             delete cc._reactiveMethodData[serializedArgs];
             delete cc._reactiveMethodStale[serializedArgs];
+            deleteMethodComputation(cc, serializedArgs);
           }
         });
+
+        if (cc.stopped) {
+          // Delete this computation from global computation store to avoid
+          // keeping a reference to every computation ever
+          cleanUpComputation(cc);
+        }
       });
     }
   },
-  invalidate: function (/* arguments */){
+
+  /**
+   * Invalidate all computations that are currently depending on the result
+   * of a particular ReactiveMethod.call.
+   */
+  invalidateCall: function (methodName /*, ...arguments */) {
     var args = _.toArray(arguments);
-    invalidate(args); //
-    invalidate(_.first(args), _.rest(args));
+    ReactiveMethod.invalidateApply(methodName, _.rest(args));
+  },
+
+  /**
+   * Invalidate all computations that are currently depending on the result of
+   * a particular ReactiveMethod.apply.
+   */
+  invalidateApply: function (methodName, methodArgs) {
+    var serializedArgs = EJSON.stringify([methodName, methodArgs]);
+
+    _.each(ReactiveMethod._computations[serializedArgs], function (cc) {
+      delete cc._reactiveMethodData[serializedArgs];
+      cc.invalidate();
+    });
   }
 };
 
-function invalidate(/* arguments */){
-  var args = _.toArray(arguments);
-  var serializedArgs = EJSON.stringify(args);
-  _.each(ReactiveMethod._computations[serializedArgs], function (cc){
-    delete cc._reactiveMethodData[serializedArgs];
-    cc.invalidate();
+/**
+ * Record that a computation is using the result of a method call, to allow
+ * invalidation from outside of the computation
+ * @param  {String} serializedArgs Arguments to Method.apply, in serialized form
+ */
+function recordMethodComputation(computation, serializedArgs) {
+  // Add computation to the list of computations using these arguments, and
+  // create the array if it doesn't exist.
+  var initial = ReactiveMethod._computations[serializedArgs] || [];
+  ReactiveMethod._computations[serializedArgs] =
+    _.union(initial, [computation]);
+}
+
+/**
+ * Remove the computation from the global dictionary of which computations are
+ * watching which method results
+ * @param  {Tracker.Computation} computation
+ * @param  {String} serializedArgs Arguments to Method.apply, in serialized form
+ */
+function deleteMethodComputation(computation, serializedArgs) {
+  var methodsForArgs = ReactiveMethod._computations[serializedArgs];
+  var withoutCC = _.without(methodsForArgs, computation);
+
+  if (withoutCC.length > 0) {
+    // Remove computation from the array
+    ReactiveMethod._computations[serializedArgs] = withoutCC;
+  } else {
+    // Delete the array if it is empty to avoid memory leak
+    delete ReactiveMethod._computations[serializedArgs];
+  }
+}
+
+/**
+ * Remove all references to the computation from global cache of computations,
+ * used to avoid memory leaks from storing stopped computations
+ * @param  {Tracker.Computation} computation
+ */
+function cleanUpComputation(computation) {
+  _.each(computation._reactiveMethodData, function (data, serializedArgs) {
+    deleteMethodComputation(computation, serializedArgs);
   });
 }
