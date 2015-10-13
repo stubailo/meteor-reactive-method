8d127c2a020d7ea284faf9fa7065dc53542c3480
Dean Radcliffe <deanmisc@yahoo.com>
2015-10-13 16:30:35 -0500

    Updating

--- package.js
+++ package.js
@@ -1,13 +1,13 @@
 Package.describe({
   name: 'deanius:reactive-method',
   version: '1.1.3',
-  summary: 'Deprecated. See okgrow:promise instead',
+  summary: 'Deprecated. See deanius:promise instead',
   git: 'https://github.com/deanius/meteor-reactive-method',
   documentation: 'README.md'
 });
 
 Package.onUse(function(api) {
-  api.versionsFrom('1.0.3.1');
+  api.versionsFrom('1.2.0.2');
   api.use(["tracker", "meteor", "ddp", "ejson"]);
-  api.imply('okgrow:promise@0.9.0');
+  api.imply('deanius:promise@3.0.1');
 });
